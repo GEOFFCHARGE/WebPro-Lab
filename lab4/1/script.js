3b3ec2da483156ff24f247e4d276cbf92f8be4bd
GEOFFCHARGE <office.wanlachai@gmail.com>
2025-12-26 13:10:56 +0700

    add lab4.1

--- lab4/1/script.js
+++ lab4/1/script.js
@@ -4,14 +4,31 @@ function multiply() {
         alert("Set a value between 1 and 12 only");
         return 0;
     }
-    let table = document.getElementById("table")
-    let tHead = table.getElementsByTagName("thead")[0];
-    let tBody = table.getElementsByTagName("tbody")[0];
-    tHead.innerHTML = "<tr><td>เลขคูณ</td><td>ผลลัพธ์</td></tr>"
-    let temp = "";
-    for (let i = 1; i < 8; i++) {
-        temp += "<tr><td>" + value + " × " + i + "</td><td>" + (value * i) + "</td></tr>";
+    if (document.getElementsByTagName("table").length) {
+        document.getElementById("multiply").removeChild(document.getElementsByTagName("table")[0]);
     }
-    tBody.innerHTML = temp;
-    return 1;
+    let table = document.createElement("table");
+    let tr = document.createElement("tr");
+    let th1 = document.createElement("th");
+    let th2 = document.createElement("th");
+    let txt1 = document.createTextNode("ผลคูณ");
+    let txt2 = document.createTextNode("ผลลัพธ์");
+    th1.appendChild(txt1);
+    th2.appendChild(txt2);
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    table.appendChild(tr);
+    for (let i = 1; i < 13; i++) {
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let txt1 = document.createTextNode(value + " × " + i);
+        let txt2 = document.createTextNode(value * i);
+        td1.appendChild(txt1);
+        td2.appendChild(txt2);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        table.appendChild(tr);
+    }
+    document.getElementById("multiply").appendChild(table);
 }
