ddce0373ee83e0aeae087046a29e055a35caafbe
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-01-22 23:54:56 +0700

    update lab6.ex

--- example/6/script.js
+++ example/6/script.js
@@ -31,28 +31,45 @@ fetch("superheroes.json")
 */
 
 /* 2c */
-/* function orders(data) {
+function orders(data) {
     document.getElementById("id").innerHTML = data.orderId;
     document.getElementById("date").innerHTML = data.orderDate;
     document.getElementById("cust").innerHTML = data.customer.name;
     document.getElementById("mail").innerHTML = data.customer.email;
     data.items.forEach(item => {
-        let td = document.createElement("td");
         let tr = document.createElement("tr");
-        let txt = document.createTextNode(item.productId);
-        td.appendChild(tr);
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        let td5 = document.createElement("td");
+        let txt1 = document.createTextNode(parseInt(item.productId.slice(5)));
+        let txt2 = document.createTextNode(item.name);
+        let txt3 = document.createTextNode(item.quantity);
+        let txt4 = document.createTextNode("$" + item.price);
+        let txt5 = document.createTextNode("$" + item.quantity * item.price);
+        td1.appendChild(txt1);
+        td2.appendChild(txt2);
+        td3.appendChild(txt3);
+        td4.appendChild(txt4);
+        td5.appendChild(txt5);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        document.getElementById("tb").appendChild(tr);
     });
-    document.getElementById("ship").innerHTML = data.shipping.address.street + " " + data.shipping.address.city + " " + data.shipping.address.state + " " + data.shipping.address.zipCode;
+    document.getElementById("ship").innerHTML = data.shipping.address.street + ", " + data.shipping.address.city + ", " + data.shipping.address.state + " " + data.shipping.address.zipCode;
 }
 
 fetch("order.json")
     .then(response => response.json())
     .then(data => orders(data))
     .catch(error => console.log("error", error));
-*/
 
 /* 3 */
-function SaveForm() {
+/* function SaveForm() {
     let fname = document.getElementById("FirstName").value;
     let lname = document.getElementById("LastName").value;
     let country = document.getElementById("Country").value;
@@ -70,4 +87,4 @@ function LoadData(){
     document.getElementById("LastName").value = ln;
     document.getElementById("Country").value = cty;
     localStorage.clear();
-}
+} */
