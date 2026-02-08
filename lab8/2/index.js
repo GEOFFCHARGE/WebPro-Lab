8e35c4b6e01bd660e4177352226c979458ed699e
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-08 18:45:00 +0700

    add lab8.2

---
+++ lab8/2/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const conn = require("./database");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/html/signin.html"));
+});
+
+app.get("/formget", (req, res) => {
+    const { identify, password } = req.query;
+    const sql = `SELECT * FROM Users WHERE username = ? OR email = ?;`;
+    conn.query(sql, [identify, identify], (err, result) => {
+        if (err) throw err;
+        else if (result.length == 0) {
+            console.log("ไม่พบบัญชีผู้ใช้");
+            res.send("ไม่พบบัญชีผู้ใช้");
+        }
+        else if (result[0].password !== password) {
+            console.log("รหัสผ่านไม่ถูกต้อง");
+            res.send("รหัสผ่านไม่ถูกต้อง");
+        }
+        else {
+            console.log("ตรงกับที่มีในฐานข้อมูล");
+            res.render("show", { data: result[0] });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+});
