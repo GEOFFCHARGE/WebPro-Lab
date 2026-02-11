4ec6c222de3dabce3d4b24ea50a4ebf53679df60
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-11 18:08:37 +0700

    update lab8.1

--- lab8/1/index.js
+++ lab8/1/index.js
@@ -9,9 +9,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    const sql =
+    const createSql =
     `CREATE TABLE IF NOT EXISTS Users (
-        username VARCHAR(20) NOT NULL PRIMARY KEY,
+        username VARCHAR(20) NOT NULL,
         password VARCHAR(20) NOT NULL,
         email VARCHAR(20) NOT NULL,
         firstname VARCHAR(20) NOT NULL,
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
         address VARCHAR(100) NOT NULL,
         phone VARCHAR(10) NOT NULL
     );`;
-    conn.query(sql, (err, result) => {
+    conn.query(createSql, (err, result) => {
         if (err) throw err;
         console.log("Table created or already exists");
         res.sendFile(path.join(__dirname, "/public/html/register.html"));
@@ -32,16 +32,14 @@ app.get("/formget", (req, res) => {
     const insertSql = "INSERT INTO Users (username, password, email, firstname, lastname, age, address, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
     conn.query(insertSql, [username, password, email, firstname, lastname, age, address, phone], (err, result) => {
         if (err) throw err;
-        console.log("Data inserted");
-        res.send("Data inserted");
+        res.redirect("/showdata");
     });
 });
 
 app.get("/showdata", (req, res) => {
-    const sql = "SELECT * FROM Users;";
-    conn.query(sql, (err, result) => {
+    const showSql = "SELECT * FROM Users;";
+    conn.query(showSql, (err, result) => {
         if (err) throw err;
-        // console.log(result);
         res.render("show", { data: result });
     });
 });
