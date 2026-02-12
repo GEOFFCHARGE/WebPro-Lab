cb41c83b1115950beca44bae8f797919d8766bdd
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-12 09:54:28 +0700

    update lab8.2

--- lab8/2/index.js
+++ lab8/2/index.js
@@ -12,6 +12,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/html/signin.html"));
 });
 
+app.get("/notfound", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/html/notfound.html"));
+});
+
+app.get("/wrongpass", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/html/wrongpass.html"));
+});
+
 app.get("/formget", (req, res) => {
     const { identify, password } = req.query;
     const sql = `SELECT * FROM Users WHERE username = ? OR email = ?;`;
@@ -19,11 +27,11 @@ app.get("/formget", (req, res) => {
         if (err) throw err;
         else if (result.length == 0) {
             console.log("ไม่พบบัญชีผู้ใช้");
-            res.send("ไม่พบบัญชีผู้ใช้");
+            res.redirect("/notfound");
         }
         else if (result[0].password !== password) {
             console.log("รหัสผ่านไม่ถูกต้อง");
-            res.send("รหัสผ่านไม่ถูกต้อง");
+            res.redirect("/wrongpass");
         }
         else {
             console.log("ตรงกับที่มีในฐานข้อมูล");
