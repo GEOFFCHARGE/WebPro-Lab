e5c140f3a2777a3726d5d7d616f2d882c1a73968
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-24 10:37:45 +0700

    add lab10.1

---
+++ lab10/1/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    const endpoint = "http://webdev.it.kmitl.ac.th:4000/restaurant";
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(wsdata => {
+            console.log(wsdata);
+            res.render("show", { data: wsdata });            
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.get("/detail/:id", (req, res) => {
+    const endpoint = "http://webdev.it.kmitl.ac.th:4000/detail/" + req.params.id;
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(wsdata => {
+            console.log(wsdata);
+            res.render("detail", { data: wsdata });            
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.listen(port, () => {
+  console.log(`Starting server at port ${port}`);
+});
