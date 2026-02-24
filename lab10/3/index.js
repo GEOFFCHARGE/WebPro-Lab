b46780104d656ebea2f7ff735ab4dd0887fbe574
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-02-24 08:56:38 +0700

    add lab10.3

---
+++ lab10/3/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const port = 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    const endpoint = "http://webdev.it.kmitl.ac.th:4000/books";
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(wsdata => {
+            console.log(wsdata);
+            res.render("show", { data: wsdata });            
+        })
+        .catch(error => {
+            console.log(error);
+        });
+});
+
+app.listen(port, () => {
+  console.log(`Starting server at port ${port}`);
+});
