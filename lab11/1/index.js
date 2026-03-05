b12f6d374ee762c6db70181700ddc7ae49abdfef
GEOFFCHARGE <office.wanlachai@gmail.com>
2026-03-05 14:11:14 +0700

    update lab11.1

--- lab11/1/index.js
+++ lab11/1/index.js
@@ -2,21 +2,23 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+
 const app = express();
 const port = 3000;
 
-let db = new sqlite3.Database("customers.db", (err) => {
+const db = new sqlite3.Database("customers.db", (err) => {
     if (err) throw err;
     console.log("Connected to the SQLite database.");
 });
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    const sql = `SELECT * FROM customers ORDER BY RANDOM() LIMIT 1`;
+    let sql = `SELECT * FROM customers ORDER BY RANDOM() LIMIT 1`;
     db.get(sql, (err, row) => {
         if (err) throw err;
         res.render("form", { data: row });
@@ -33,7 +35,8 @@ app.get("/set-cookie", (req, res) => {
         res.render("form", { data: {} });
     }
     catch (err) {
-        res.status(500).send("Error setting cookie");
+        console.error('Error setting cookie:', err);
+        res.status(500).send('Internal Server Error');
     }
 });
 
@@ -42,7 +45,8 @@ app.get("/get-cookie", (req, res) => {
         res.render("form", { data: req.cookies.info || {} });
     }
     catch (err) {
-        res.status(500).send("Error reading cookie");
+        console.error('Error reading cookies:', err);
+        res.status(500).send('Internal Server Error');
     }
 });
 
